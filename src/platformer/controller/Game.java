3f4e355a889337546af21eaa1dadeab519fd4183
Bryce <bhyer06@Gmail.com>
2023-04-30 16:34:03 -0600

    I am about to make the character move using booleans

--- src/platformer/controller/Game.java
+++ src/platformer/controller/Game.java
@@ -1,5 +1,8 @@
 package platformer.controller;
 
+import java.awt.Graphics;
+
+import entities.Player;
 import platformer.view.GamePanel;
 import platformer.view.GameWindow;
 
@@ -12,16 +15,26 @@ public class Game implements Runnable
 	private final int FPS_SET = 120;
 	private final int UPS_SET = 200;
 	
+	private Player player;
+	
 	public Game()
 	{
-		gamePanel = new GamePanel();
+		initClasses();
+		gamePanel = new GamePanel(this);
 		gameWindow = new GameWindow(gamePanel);
 		gamePanel.requestFocus();
 		startGameLoop();
+
 		
 	}
 	
 	
+	private void initClasses() {
+		player = new Player(200, 200);
+		
+	}
+
+
 	private void startGameLoop()
 	{
 		gameThread = new Thread(this);
@@ -30,9 +43,14 @@ public class Game implements Runnable
 	
 	public void update()
 	{
-		gamePanel.updateGame();
+		player.update();
 	}
-
+	
+	public void render(Graphics g)
+	{
+		player.render(g);
+	}
+	
 	@Override
 	public void run() 
 	{
@@ -91,7 +109,10 @@ public class Game implements Runnable
 		
 	}
 	
-	
+	public Player getPlayer()
+	{
+		return player;
+	}
 	
 	
 	
--- src/platformer/view/GamePanel.java
+++ src/platformer/view/GamePanel.java
@@ -16,6 +16,7 @@ import javax.swing.event.AncestorListener;
 
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
+import platformer.controller.Game;
 
 import static utilz.Constants.PlayerConstants.*;
 import static utilz.Constants.Directions.*;
@@ -29,89 +30,18 @@ public class GamePanel extends JPanel
 {
 	
 	private MouseInputs mouseInputs;
-	private float xDelta = 100;
-	private float yDelta = 100;
-	private int frames = 0;
-	private long lastCheck = 0;
-	private BufferedImage img;
-	private BufferedImage[][] animations;
-	private int aniTick;
-	private int aniIndex;
-	private int aniSpeed = 15;
-	private int playerAction = IDLE;
-	private int playerDir = -1;
-	private boolean moving = false;
+	private Game game;
 	
-	
-	
-	
-	public GamePanel()
+	public GamePanel(Game game)
 	{
-		
 		mouseInputs = new MouseInputs(this);
-		
-		importImg();
-		loadAnimations();
-		
+		this.game = game;
 		setPanelSize();
 		this.addKeyListener(new KeyboardInputs(this));
 		this.addMouseListener(mouseInputs);
 		this.addMouseMotionListener(mouseInputs);
 		
 	}
-	
-	
-	
-	private void loadAnimations() 
-	{
-		animations = new BufferedImage[9][6];
-		
-		
-		for (int j = 0; j < animations.length; j++)
-		{
-			for (int i = 0; i < animations[j].length; i++)
-			{
-				animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
-			}
-		}
-		
-		
-	}
-
-
-
-	private void importImg()   
-	{
-		InputStream is = getClass().getResourceAsStream("/player_sprites.png");
-		
-		try 
-		{
-			img = ImageIO.read(is);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			try 
-			{
-				is.close();
-			}
-			catch(IOException e) 
-			{
-				e.printStackTrace();
-			}
-			{
-				
-			}
-		}
-			
-		
-		
-		}
-
-
 
 	private void setPanelSize() 
 	{
@@ -121,104 +51,22 @@ public class GamePanel extends JPanel
 		this.setMaximumSize(size);
 		
 	}
-
-
-	public void setDirection(int direction) 
-	{
-		this.playerDir = direction;
-		moving = true;
-	}
-	
-	public void setMoving(boolean moving)
-	{
-		this.moving = moving;
-	}
 	
 	public void updateGame()
 	{
-		updateAnimationTick();
-		setAnimation();
-		updatePostion();
+		
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
 		
-		
-		g.drawImage(animations[playerAction][aniIndex], (int) xDelta, (int) yDelta, 256, 160, null);
-		
-		
-		
-		
-		
-		
+		game.render(g);
 	}
-
-
-
-	private void updatePostion() 
+	
+	public Game getGame()
 	{
-		if (moving)
-		{
-			switch (playerDir)
-			{
-			case LEFT:
-				xDelta -= 5;
-				break; 
-			case UP:
-				yDelta -= 5;
-				break;
-			case RIGHT:
-				xDelta += 5;
-				break;
-			case DOWN:
-				yDelta += 5;
-				break;
-			}
-		}
-		
+		return game;
 	}
-
-
-
-	private void setAnimation() 
-	{
-		if (moving)
-		{
-			playerAction = RUNNING;
-		}
-		else
-		{
-			playerAction = IDLE;
-		}
-		
-	}
-
-
-
-	private void updateAnimationTick() 
-	{
-		
-		aniTick++;
-		if (aniTick >= aniSpeed)
-		{
-			aniTick = 0;
-			aniIndex++;
-			if (aniIndex >= GetSpriteAmount(playerAction))
-			{
-				aniIndex = 0;
-			}
-			
-		}
-		
-	}
-
-
-
-
-
-
-
 	
 }
